13155c39b05f078ee74259f876cbaf8d048792d7
Fabio Bione <fabiolopesbione@hotmail.com>
2019-05-10 18:24:59 -0300

    Add files via upload
    Commit inicial

---
+++ TDDCasaDoCodigo/NumerosRomanos/src/main/java/com/casadocodigo/ConversorDeNumeroRomano.java
@@ -0,0 +1,28 @@
+package com.casadocodigo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ConversorDeNumeroRomano {
+
+	@SuppressWarnings("serial")
+	private static final Map<Character, Integer> tabela = 
+			new HashMap<Character, Integer>(){{
+				put('I', 1);
+				put('V', 5);
+				put('X', 10);
+				put('L', 50);
+				put('C', 100);
+				put('D', 500);
+				put('M', 1000);
+			}};	
+	
+	public int converte(String numeroEmRomano){
+		int acumulador = 0;
+		for(int i = 0; i < numeroEmRomano.length(); i++){
+			acumulador += tabela.get(numeroEmRomano.charAt(i));
+		}
+		return acumulador;
+	}
+	
+}
---
+++ TDDCasaDoCodigo/NumerosRomanos/src/test/java/com/casadocodigo/ConversorDeNumeroRomanoTest.java
@@ -0,0 +1,49 @@
+package com.casadocodigo;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ConversorDeNumeroRomanoTest {
+
+	@Test
+	public void deveEntenderOSimboloI(){
+		ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+		int numero = romano.converte("I");
+		System.out.println("deveEntenderOSimboloI:" + numero);
+		assertEquals(1, numero);
+	}
+	
+	@Test
+	public void deveEntenderOSimboloV(){
+		ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+		int numero = romano.converte("V");
+		System.out.println("deveEntenderOSimboloV:" + numero);
+		assertEquals(5, numero);
+	}
+	
+	@Test
+	public void deveEntenderDoisSimbolosComoII(){
+		ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+		int numero = romano.converte("II");
+		System.out.println("deveEntenderDoisSimbolosComoII:" + numero);
+		assertEquals(2, numero);
+	}
+	
+	@Test
+	public void deveEntenderQuatroSimbolosDoisADoisComoXXII(){
+		ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+		int numero = romano.converte("XXII");
+		System.out.println("deveEntenderQuatroSimbolosDoisADoisComoXXII:" + numero);
+		assertEquals(2, numero);
+	}
+	
+	@Test
+	public void deveEntenderSimbolosComoIX(){
+		ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+		int numero = romano.converte("IX");
+		System.out.println("deveEntenderSimbolosComoIX:" + numero);
+		assertEquals(9, numero);
+	} 	
+	
+}
